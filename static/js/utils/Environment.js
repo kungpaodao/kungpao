1fe0e64a987fcc27dd6a34f7a9b770a7219f53f9
KungPaoDao <91539597+squidswapnft@users.noreply.github.com>
2022-07-03 15:42:23 -0400

    Add files via upload

---
+++ static/js/utils/Environment.js
@@ -0,0 +1,6 @@
+let Environment = {
+    YfethContractAddress: "0x2A11149C59FCbdCDcA4680ed19f25C12AA9427D8",
+    MorocoContractcAddress: "0x2A11149C59FCbdCDcA4680ed19f25C12AA9427D8",
+    evangelismAddress: "0x2A11149C59FCbdCDcA4680ed19f25C12AA9427D8",
+  };
+  export default Environment;
